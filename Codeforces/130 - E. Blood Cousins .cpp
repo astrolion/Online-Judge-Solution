f0f65ca653f07748cbdc5768d01088891acba6a2
Ashini Singha <aslitroon@gmail.com>
2019-09-21 21:21:24 +0600

    #init, stage problem..

---
+++ Codeforces/130 - E. Blood Cousins .cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+const int N = 1e5 + 5;
+int parent[N];
+int n, m;
+
+int main() {
+
+        scanf("%d", &n);
+
+        for(int i = 1; i <= n; i++) {
+                scanf("%d", &parent[i]);
+        }
+
+        scanf("%d", &m);
+
+        while(m--) {
+                int node, pos; scanf("%d %d", &node, &pos);
+        }
+
+
+        return 0;
+}
