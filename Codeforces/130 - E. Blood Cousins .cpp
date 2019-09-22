dea4374b81b929303e343b1d12a270df81ef7ad8
Ashini Singha <aslitroon@gmail.com>
2019-09-22 10:43:01 +0600

    #checking, depth..

--- Codeforces/130 - E. Blood Cousins .cpp
+++ Codeforces/130 - E. Blood Cousins .cpp
@@ -5,7 +5,25 @@ const int N = 1e5 + 5;
 int parent[N];
 int n, m;
 
+vector< int > g[N];
+
+void dfs(int node, int dep) {
+
+        cout << node << " " << dep << endl;
+
+        for(auto x : g[node]) {
+                dfs(x, dep + 1);
+        }
+}
+
 int main() {
+
+        g[0] = {1, 4};
+        g[1] = {2, 3};
+        g[4] = {5, 6};
+
+        dfs(0,0);
+
 
         scanf("%d", &n);
 
@@ -17,6 +35,9 @@ int main() {
 
         while(m--) {
                 int node, pos; scanf("%d %d", &node, &pos);
+
+
+
         }
 
 
